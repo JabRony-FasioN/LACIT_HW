41343842f5b809fef55b4a3cfd8841b792e03e4a
JabRony-FasioN <ahiamsorry@mail.ru>
2021-04-06 15:21:48 +0300

    Create first_doubt.py

---
+++ first_doubt.py
@@ -0,0 +1,16 @@
+winter = [1, 2, 12]
+sprenn = [3, 4, 5]
+summer = [6, 7, 8]
+autumn = [9, 10, 11]
+a = input('ifspokf:')
+
+if a in winter:
+    print("зима")
+elif a in sprenn:
+    print("весна")
+elif a in summer:
+    print("лето")
+elif a in autumn:
+    print("осень")
+else:
+    print("ошибка")            
