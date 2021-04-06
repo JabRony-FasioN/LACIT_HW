4658b2293bf622be92fb5fbccbcc8253c07515fa
JabRony-FasioN <ahiamsorry@mail.ru>
2021-04-06 15:21:59 +0300

    Create homework1_1.py

---
+++ homework1_1.py
@@ -0,0 +1,14 @@
+L = [ 1, -2, 10, 13, 51, 1010, 9000]
+x = []
+for i in L:
+    if i % 2 == 0:
+        x.append(i)
+print(x)        
+
+
+
+#for i in L:
+ #   if int(L[i]) % 2 == 0:
+  #      print(L[i])
+    
+        
